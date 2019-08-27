3ab454372766310229816ae3606dcac760805013
ALYSSON EDUARDO SANTOS ARAUJO <1090481911048@fatec.local>
2019-08-27 10:56:00 -0300

    lista2

--- function.js
+++ function.js
@@ -101,3 +101,40 @@ function exercicio7(){
 
     }
 }
+function exercicio22(){
+    var idade = Number(document.getElementById("idade").value);
+    var peso = Number(document.getElementById("peso").value);
+    var risco = 0
+    if (idade < 20){
+        if (peso < 60){
+            risco = 9
+        }
+        else if (peso > 60 && peso <90)
+            risco = 8 
+        else {
+            risco = 7
+        }
+    }
+    else if  (idade > 20 && idade < 50) 
+        if (peso < 60){
+            risco = 6
+        }
+        else if (peso > 60 && peso < 90){
+            risco = 5
+        }
+        else{
+            risco = 4 
+        }
+    else {
+        if (peso < 60) {
+            risco = 3 
+        }
+        else if  (peso > 60 && peso < 90){
+            risco = 2
+        }
+        else{ 
+            risco = 1
+        } 
+}
+    alert("Você pertence ao grupo: " + risco)
+}
